1bea3ba022109fbefa56bf672d2c2af31b9f5e09
jithendrabathala <jithendrabathala@gmail.com>
2025-02-09 21:47:31 +0530

    swager docs

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Express } from 'express';
 import { createServer, Server } from 'http';
+import YAML from 'yaml';
+import fs from 'fs';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
 
 import rootRouter from './routes/index.routes';
 import ErrorHandler from './middlewares/error';
+
+const file = fs.readFileSync(path.resolve('./swagger.yaml'), 'utf8');
+const swaggerDocument = YAML.parse(file);
 
 const app: Express = express();
 
@@ -17,5 +24,16 @@ app.use(cookieParser());
 app.use('/api', rootRouter);
 
 app.use(ErrorHandler);
+
+app.use(
+  '/swagger',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      docExpansion: 'none',
+    },
+    customSiteTitle: 'Time Sheet API docs',
+  }),
+);
 
 export default httpServer;
